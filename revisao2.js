9a55f135b9b9138deab0c6ea69b4a77d229a8989
ana julia goncalves <ana.j.goncalves13@aluno.senai.br>
2026-03-03 14:25:42 -0300

    Atividade finalizada por ana julia

--- revisao2.js
+++ revisao2.js
@@ -1,7 +1,14 @@
-let produtos = ["Arroz", "Feijão", "Açúcar", "Macarrão", "Amaciante"];
 
+let produtos = ["Arroz", "Açúcar", "Feijão", "Macarrão", "Amendoim"];
+
+console.log("Lista completa:");
 for (let i = 0; i < produtos.length; i++) {
-  if (produtos[i].startsWith("A")) {
+  console.log(produtos[i]);
+}
+
+console.log("Produtos que começam com a letra A:");
+for (let i = 0; i < produtos.length; i++) {
+  if (produtos[i][0] === "A") {
     console.log(produtos[i]);
   }
 }
